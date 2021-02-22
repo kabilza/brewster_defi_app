6a032b05f71ffb487ea1a75383ce59290e0fd0d7
kabilza <naruoluvohina@gmail.com>
2021-02-23 00:33:42 +0700

    initial commit

---
+++ src/contracts/TokenFarm.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.5.0;
+
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
+contract TokenFarm {
+    string public name = "Dapp Token Farm";
+    address public owner;
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+        owner = msg.sender;
+    }
+
+    function stakeTokens(uint _amount) public {
+        require(_amount > 0, "amount cannot be 0");
+
+
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    // Unstaking Tokens (Withdraw)
+    function unstakeTokens() public {
+        // Fetch staking balance
+        uint balance = stakingBalance[msg.sender];
+
+
+        require(balance > 0, "staking balance cannot be 0");
+
+
+        daiToken.transfer(msg.sender, balance);
+
+ 
+        stakingBalance[msg.sender] = 0;
+
+
+        isStaking[msg.sender] = false;
+    }
+
+    // Issuing Tokens
+    function issueTokens() public {
+
+        require(msg.sender == owner, "caller must be the owner");
+
+        // Issue tokens to all stakers
+        for (uint i=0; i<stakers.length; i++) {
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if(balance > 0) {
+                dappToken.transfer(recipient, balance);
+            }
+        }
+    }
+}
